40a170d4ff30f117e3532bfcef88f15f3081f9ac
Marcus <marcus.shimotsu@gmail.com>
2021-12-22 17:03:21 -0600

    commit tree 1d0c08744df2f3286e9df18f519b9f97bc124fbb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,38 @@
 class Movie < ActiveRecord::Base
+    def self.create_with_title(title)
+        self.create(title: title)
+    end
+
+    def self.first_movie
+        self.first
+    end
+    
+    def self.last_movie
+        self.last
+    end
+
+    def self.movie_count
+        self.count
+    end
+
+    def self.find_movie_with_id(id)
+        self.find(id)
+    end
+
+    def self.delete_by_id(id)
+        self.destroy(id)
+    end
+
+    def self.delete_all_movies
+        self.destroy_all
+    end
+
+    def self.find_movie_with_attributes(title:,release_date:)
+        self.find_by(title: title,release_date: release_date)
+    end
+    
+    def update_with_attributes(title:,release_date:)
+        self.update(title: title,release_date: release_date)
+    end
 
 end
